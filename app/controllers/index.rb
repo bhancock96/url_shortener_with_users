2e7506b88838c4fe1d584eb82881a0642cf5042e
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 16:15:05 -0500

    finished

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,43 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get "/create" do 
+
+  erb :create_user
+end 
+
+get "/secret_page" do
+  if current_user 
+    erb :secret
+  else
+    @error = "you gotta login, ya chancho."
+    erb :index
+  end 
+
+end 
+
+get "/users/logout" do
+  session.clear
+  redirect '/'
+end
+
+
+#----POST---------------------------------------------------------
+
+post "/login" do 
+  puts params
+  @user = User.login(params)
+  session[:user_id] = @user.id
+  puts session[:user_id]
+  puts 
+  puts 
+  redirect "/secret_page"
+end 
+
+post "/create_user" do 
+  @user = User.create(params[:user])
+  session[:user_id] = @user.id 
+  redirect '/secret_page'
+end 
+
---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_uniqueness_of :email
+
+   include BCrypt
+
+    def password
+      @password ||= Password.new(password_hash)
+    end
+
+    def password=(new_password)
+      @password = Password.create(new_password)
+      self.password_hash = @password
+    end
+
+    def self.login(params) 
+      @user = User.find_by_email(params[:user][:email])
+      if @user.password == params[:user][:password]
+        @user
+        # session[:user_id] = @user.id
+      else
+        redirect '/' 
+      end
+    end
+
+end
